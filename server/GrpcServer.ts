65c3b4c616ba7071f783b99e8b7c7baa93686867
williamwoldum <wwoldu21@student.aau.dk>
2024-03-22 17:09:07 +0100

    Initial

---
+++ server/GrpcServer.ts
@@ -0,0 +1,46 @@
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+import { Empty } from '../proto/google/protobuf/Empty'
+import { ProtoGrpcType } from '../proto/api'
+import { JPoolAPIHandlers } from '../proto/protobuf/JPoolAPI'
+import { ShowShotsRequest } from '../proto/protobuf/ShowShotsRequest'
+import { WebSocketServer } from 'ws'
+
+const PROTO_PATH = __dirname + '/../fastfiz-renderer-protobuffers/api.proto'
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+})
+const api_proto = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
+
+function ShowShots(call: grpc.ServerUnaryCall<ShowShotsRequest, Empty>, callback: grpc.sendUnaryData<Empty>) {
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(call.request.shots))
+  })
+
+  callback(null, {
+    message: `Shots received`,
+  })
+}
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error)
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data)
+  })
+})
+
+const grpcServer = new grpc.Server()
+const service: JPoolAPIHandlers = { ShowShots: ShowShots }
+
+grpcServer.addService(api_proto.protobuf.JPoolAPI.service, service)
+grpcServer.bindAsync('0.0.0.0:50052', grpc.ServerCredentials.createInsecure(), () => {
+  grpcServer.start()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
-import "./App.css";
-import Geogebra, { Props as GGParams } from "react-geogebra";
-import GrpcServer from "./GrpcServer";
+import { useEffect } from 'react'
+import Geogebra, { Props as GGParams } from 'react-geogebra'
+import { Shot } from '../proto/protobuf/Shot'
 
-const params: GGParams = {
-	id: "app",
-	width: 800,
-	height: 600,
-	showMenuBar: true,
-	showToolBar: false,
-	showAlgebraInput: true,
-	appletOnLoad: () => {}
-};
-
-const server = new GrpcServer(window.app);
-server.start("0.0.0.0", 50052);
-
-function App() {
-	return <Geogebra {...params} />;
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    app: any
+  }
 }
 
-export default App;
+const params: GGParams = {
+  id: 'app',
+  width: 800,
+  height: 600,
+  showMenuBar: true,
+  showToolBar: false,
+  showAlgebraInput: true,
+  appletOnLoad: () => {},
+}
+
+window.app = {}
+
+window.onkeyup = (e: KeyboardEvent) => {
+  if (e.key === 'Enter') {
+    const app = window.app
+    const input = app.getAlgebraInput()
+    app.evalCommand(input)
+  }
+}
+
+function App() {
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080/ws')
+    newSocket.onopen = () => console.log('connected')
+    newSocket.onclose = () => console.log('disconnected')
+
+    newSocket.onmessage = (e) => {
+      const shots: Shot[] = JSON.parse(e.data)
+      drawShot(shots[0])
+    }
+  }, [])
+
+  return <Geogebra {...params} />
+}
+
+function drawShot(shot: Shot) {
+  const app = window.app
+  app.evalCommand(`A = (${shot.rightMost?.x}, ${shot.rightMost?.y})`)
+}
+
+export default App
